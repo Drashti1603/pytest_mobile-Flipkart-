332ca022e67ce853e163f7d7b4628733617f2d6b
Drashti1603 <20it017@charusat.edu.in>
2024-03-04 17:38:08 +0530

    15th commit

--- setup_and_run.sh
+++ setup_and_run.sh
@@ -26,7 +26,7 @@ else
     exit 1
 fi
 nvm install node
-echo Volansys@2023 | sudo -S apt install nodejs
+echo 160302 | sudo -S apt install nodejs
 npm install -g appium
 
 # Run the Robot Framework test suite specified in the requirements.txt file
